4a1f767e933c7f30f1e03ed222afc6c81e6dc3a2
Maxime Caruchet <maxime.caruchet@corp.ovh.com>
2020-04-28 15:34:52 +0000

    vpc test 2

--- vpc.tf
+++ vpc.tf
@@ -1,4 +1,19 @@
 module "vpc" {
-  source  = "app.terraform.io/maxime-training/vpc/aws"
+  source = "app.terraform.io/maxime-training/vpc/aws"
   version = "2.33.0"
+
+  name = "simple-example"
+
+  cidr = var.address_space
+
+  azs             = ["us-east-1"]
+
+  tags = {
+    Owner       = "test"
+    Environment = "private-module"
+  }
+
+  vpc_tags = {
+    Name = "my-vpc-custom"
+  }
 }
